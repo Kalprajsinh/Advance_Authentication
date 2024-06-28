fa4492201e792d1ef644c3ad43d42a3f66ad1e2f
Kalprajsinh <kalpraj51@gmail.com>
2024-06-28 17:05:00 +0530

    frontend

---
+++ Frontend/src/App.tsx
@@ -0,0 +1,16 @@
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className=" text-lime-600 font-bold  text-normal">
+        Welcome to Tailwind CSS Installation Tutorial.
+      </h1>
+
+      
+    </>
+  )
+}
+
+export default App
