ed862d05453277901f6c5b01b0de9b6fb26d76e7
Kalprajsinh <kalpraj51@gmail.com>
2024-07-02 17:25:47 +0530

    check valid user

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,7 +1,50 @@
+import { useEffect } from "react";
 import Main from "./Components/Main"
 import { BrowserRouter as Router } from 'react-router-dom';
 
+
+
+
 function App() {
+  
+  // const ctrlShiftKey = (e: KeyboardEvent, keyCode: string) => {
+  //   return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+  // };
+  // useEffect(() => {
+  //   // Disable right-click context menu
+  //   document.addEventListener('contextmenu', (e: Event) => {e.preventDefault() });
+  //   // Disable specified key combinations
+  //   document.addEventListener('keydown', (e: KeyboardEvent) => {
+  //     // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  //     if (
+  //       e.keyCode === 123 ||
+  //       ctrlShiftKey(e, 'I') ||
+  //       ctrlShiftKey(e, 'J') ||
+  //       ctrlShiftKey(e, 'C') ||
+  //       (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  //     ) {
+  //       e.preventDefault();
+  //     }
+  //   });
+
+  //   return () => {
+  //     // Clean up event listeners when component unmounts
+  //     document.removeEventListener('contextmenu', (e: Event) => {e.preventDefault() });
+  //     document.removeEventListener('keydown', (e: KeyboardEvent) => {
+  //       // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+  //       if (
+  //         e.keyCode === 123 ||
+  //         ctrlShiftKey(e, 'I') ||
+  //         ctrlShiftKey(e, 'J') ||
+  //         ctrlShiftKey(e, 'C') ||
+  //         (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))
+  //       ) {
+  //         e.preventDefault();
+  //       }
+  //     });
+  //   };
+  // }, []); // Empty dependency array ensures effect runs only once
+
   return (
     <Router>
       <Main />
