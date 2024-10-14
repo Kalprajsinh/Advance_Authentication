fefaa460a9458bdc29549d635aa16530a0b48e72
Kalprajsinh <96036005+Kalprajsinh@users.noreply.github.com>
2024-10-14 23:38:14 +0530

    Update Access.js

--- Backend/controllers/Access.js
+++ Backend/controllers/Access.js
@@ -7,7 +7,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-    origin: 'https://advance-authentication.pages.dev', 
+    origin: 'https://advanceauthentication.pages.dev', 
     credentials: true
 }));
 
